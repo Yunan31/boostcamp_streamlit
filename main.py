9286e27cc155169e035cb5c7a1389a34a89b9887
Yunan31 <sonyh04@naver.com>
2023-12-28 15:14:35 +0900

    Feat: project init setting

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def run():
+    st.title("Hello World")
+    st.write("This is my first Streamlit app")
+
+
+if __name__ == "__main__":
+    run()
