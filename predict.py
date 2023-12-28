c346e7ce51e1d183f60194c42faa77177e7c219a
Yunan31 <sonyh04@naver.com>
2023-12-28 17:25:23 +0900

    Feat: predict function

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import os
 import gdown
 import torch
 from model import Model
+import transformers
 
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
@@ -14,3 +15,14 @@ def load_model():
 
     model = torch.load('model.pt', map_location=torch.device('cpu'))
     return model
+
+
+def predict(model, sentence_1, sentence_2):
+    model.eval()
+    tokenizer = transformers.AutoTokenizer.from_pretrained(config['model_name'])
+
+    input = tokenizer(sentence_1, sentence_2, padding=True, truncation=True, return_tensors='pt').to('cpu')
+    output = model(input['input_ids'])
+    pred = output.detach().numpy().item()
+
+    return round(pred, 1)
